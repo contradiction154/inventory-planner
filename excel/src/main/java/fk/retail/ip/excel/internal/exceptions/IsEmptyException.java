846c3fbf138fb98fec9353f278ea6751b89dad29
contradiction <agarwal.vaibhav@MT-C1MLLNNDDTY3.local>
2017-06-03 16:05:38 +0530

    adding left file

---
+++ excel/src/main/java/fk/retail/ip/excel/internal/exceptions/IsEmptyException.java
@@ -0,0 +1,10 @@
+package fk.retail.ip.excel.internal.exceptions;
+
+/**
+ * Created by agarwal.vaibhav on 02/06/17.
+ */
+public class IsEmptyException extends Exception {
+    public IsEmptyException(String message) {
+        super(message);
+    }
+}
